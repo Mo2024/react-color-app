46e8af2572814b9d937d6b8537e571f41b0bfe33
Mo2024 <mohdosama2025@gmail.com>
2022-06-16 18:29:54 +0300

    Palette component added

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Palette() {
+    return (
+        <div className='Palette'>
+            {/* navbar */}
+            <div className='Palette-colors'></div>
+            {/* footer */}
+        </div>
+    );
+};
+
+export default Palette;
