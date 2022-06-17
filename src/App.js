21474f60564a9b95102e79c911a3e6fa49f02031
Mo2024 <mohdosama2025@gmail.com>
2022-06-17 04:14:02 +0300

    Shade generation finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Palette from "./Palette";
 import seedColors from './seedColors'
 import './App.css'
+import { generatePalette } from './colorHelpers'
 
 function App() {
 
+  console.log(generatePalette(seedColors[4]))
   return (
     <div className="App">
       <Palette {...seedColors[4]} />
