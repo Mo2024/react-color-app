a08189249b245467b93e39879c24a52fb64d4f62
Mo2024 <mohdosama2025@gmail.com>
2022-08-19 10:18:56 +0300

    almost done with refactoring

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -58,12 +58,73 @@ const styles = {
             transition: '0.5s'
 
         }
+    },
+    BoxContent: {
+        position: 'absolute',
+        width: '100%',
+        left: '0px',
+        bottom: '0px',
+        padding: '10px',
+        color: 'black',
+        letterSpacing: '1px',
+        textTransform: 'uppercase',
+        fontSize: '12px',
+    },
+    CopyOverlay: {
+        opacity: '0',
+        zIndex: '0',
+        width: "100%",
+        height: "100%",
+        transition: "transform 0.6s ease-in-out",
+        transform: "scale(0.1)",
+    },
+    ShowOverlay: {
+        opacity: '1',
+        transform: 'scale(50)',
+        zIndex: '10',
+        position: 'absolute',
+    },
+    copyMessage: {
+        position: 'fixed',
+        left: '0',
+        right: '0',
+        top: '0',
+        bottom: '0',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        fontSize: '4rem',
+        transform: 'scale(0.1)',
+        opacity: '0',
+        color: 'white',
+        "& h1": {
+            fontWeight: '400',
+            textShadow: '1px 2px black',
+            background: 'rgba(255, 255, 255, 0.2)',
+            width: '100%',
+            textAlign: 'center',
+            marginBottom: '0',
+            padding: '1rem',
+            textTransform: 'uppercase',
+        },
+        '& p': {
+            fontSize: '2rem',
+            fontWeight: '100',
+        }
+    },
+    showMessage: {
+        opacity: '1',
+        transform: 'scale(1)',
+        zIndex: '25',
+        transition: 'all 0.4s ease-in-out',
+        transitionDelay: '0.3s',
     }
 
 }
 
 function ColorBox(props) {
-    let { background, name, id, paletteId, showingFullPalette, classes } = props;
+    let { background, name, id, paletteId, showingFullPalette, classes, goBack } = props;
     const [copied, setCopied] = useState(false);
 
     let changeCopyState = useCallback(() => {
@@ -75,16 +136,18 @@ function ColorBox(props) {
         e.stopPropagation()
     })
 
+
     return (
         <CopyToClipboard text={background} onCopy={changeCopyState}>
             <div style={{ background: background }} className={classes.ColorBox} >
-                <div style={{ background: background }} className={`copy-overlay ${copied ? "show" : ""}`} />
-                <div className={`copy-message ${copied ? "show" : ""}`}>
+                <div style={{ background: background }} className={`${classes.CopyOverlay} ${copied && classes.ShowOverlay}`} />
+
+                <div className={`${classes.copyMessage} ${copied && classes.showMessage}`}>
                     <h1>Copied!</h1>
                     <p className={classes.copyText}>{background}</p>
                 </div>
-                <div className='copy-container'>
-                    <div className='box-content'>
+                <div>
+                    <div className={classes.BoxContent}>
                         <span className={classes.colorName}>{name}</span>
                     </div>
                     <button className={classes.copyButton}>Copy</button>
